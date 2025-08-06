3d042a624057d3fe84626faa46e43db75c3ce235
smy619 <solene.s2025@gmail.com>
2025-08-06 18:20:54 +0200

    Initial commit

---
+++ src/datas/plantList.js
@@ -0,0 +1,96 @@
+import monstera from '../assets/monstera.jpg'
+import lyrata from '../assets/lyrata.jpg'
+import pothos from '../assets/pothos.jpg'
+import succulent from '../assets/succulent.jpg'
+import olivier from '../assets/olivier.jpg'
+import basil from '../assets/basil.jpg'
+import mint from '../assets/mint.jpg'
+import calathea from '../assets/calathea.jpg'
+import cactus from '../assets/cactus.jpg'
+
+export const plantList = [
+	{
+		name: 'monstera',
+		category: 'classique',
+		id: '1ed',
+		price: 8,
+		light: 2,
+		water: 3,
+		cover: monstera
+	},
+	{
+		name: 'ficus lyrata',
+		category: 'classique',
+		id: '2ab',
+		price: 8,
+		light: 3,
+		water: 1,
+		cover: lyrata
+	},
+
+	{
+		name: 'pothos argenté',
+		category: 'classique',
+		id: '3sd',
+		price: 6,
+		light: 1,
+		water: 2,
+		cover: pothos
+	},
+	{
+		name: 'calathea',
+		category: 'classique',
+		id: '4kk',
+		price: 7,
+		light: 2,
+		water: 3,
+		cover: calathea
+	},
+	{
+		name: 'olivier',
+		category: 'extérieur',
+		id: '5pl',
+		price: 15,
+		light: 3,
+		water: 1,
+		cover: olivier
+	},
+
+	{
+		name: 'cactus',
+		category: 'plante grasse',
+		id: '8fp',
+		price: 5,
+		light: 2,
+		water: 1,
+		cover: cactus
+	},
+	{
+		name: 'basilique',
+		category: 'extérieur',
+		id: '7ie',
+		price: 4,
+		light: 2,
+		water: 3,
+		cover: basil
+	},
+	{
+		name: 'succulente',
+		category: 'plante grasse',
+		id: '9vn',
+		price: 5,
+		light: 2,
+		water: 1,
+		cover: succulent
+	},
+
+	{
+		name: 'menthe',
+		category: 'extérieur',
+		id: '6uo',
+		price: 3,
+		light: 2,
+		water: 2,
+		cover: mint
+	}
+]
